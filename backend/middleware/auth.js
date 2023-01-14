cb88cd0752e433d6b8d06735e0c75925ca1f97c2
dereSundayT <toluwanisundaydere@gmail.com>
2023-01-14 16:47:33 +0100

    worked on method to login user

---
+++ backend/middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+const appConfig = require('../config/config')
+
+
+module.exports = function (req,res,next) {
+    // Get the token from the header
+    const token = req.header('x-auth-token')
+    //Check if no tokeN
+    if(token){
+        return res.status(401).json({msg:'UnAuthorized'})
+    }
+    //veriy taken
+    try{
+        const decoded = jwt.verify(token,appConfig.jwtSecret)
+        req.user = decoded.user;
+        next()
+    }
+    catch (e){
+        return res.status(401).json({msg:'Token is not valid'})
+    }
+}
--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -1,16 +1,77 @@
 const express = require('express')
 const router = express.Router()
-
+const auth = require('../../middleware/auth')
+const User = require('../../models/User')
+const {check, validationResult} = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const appConfig = require("../../config/config");
 
 
 /**
  * @route GET api/auth
  * @desc  Test Route
+ * @access Private
+ */
+router.get('/', auth, async (req, res) => {
+    //
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+
+    } catch (e) {
+        res.status(401).json('error')
+    }
+
+})
+
+
+/**
+ * @route GET api/auth
+ * @desc  Login User
  * @access Public
  */
-router.get('/',(req,res) => {
-    //
-    res.json('hello')
-})
+router.post('/',
+    [
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Please enter password').exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()})
+        }
+        let {email, password} = req.body
+        try {
+            //see if the use exist
+            let user = await User.findOne({email})
+            if (!user) {
+                return res.status(400).json({
+                    errors: [{msg: 'Invalid Credentials'}]
+                })
+            }
+            const isMatch = await bcrypt.compare(password, user.password)
+            if (!isMatch) {
+                return res.status(400).json({
+                    errors: [{msg: 'Invalid Credentails'}]
+                })
+            }
+
+            //Return json web token
+            const payload = {user: {id: user.id}}
+            jwt.sign(payload,
+                appConfig.jwtSecret,
+                {expiresIn: 360000000},
+                (err, token) => {
+                    if (err) throw  err;
+                    res.json({token})
+                }
+            )
+        } catch (e) {
+            console.error(e.message)
+            res.status(500).send('Server error')
+        }
+    })
 
 module.exports = router;
