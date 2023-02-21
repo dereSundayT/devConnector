01cc0a882f1688996467100e5cb26965f330ceb9
dereSundayT <toluwanisundaydere@gmail.com>
2023-02-21 14:33:41 +0100

    worked on adding and deleting  comments

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -174,4 +174,78 @@ router.put('/unlike/:id', auth, async (req, res) => {
 		})
 	}
 })
+
+/**
+ * @route POST api/posts/comment/:id
+ * @desc  Comment on  a POst
+ * @access Private
+ */
+router.post(
+	'/comment/:id',
+	[auth, [check('text', 'Text is required').not().isEmpty()]],
+	async (req, res) => {
+		const errors = validationResult(req)
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() })
+		}
+
+		try {
+			const user = await User.findById(req.user.id).select('-password')
+			const post = await Post.findById(req.params.id)
+
+			const newComment = {
+				text: req.body.text,
+				name: user.name,
+				avatar: user.avatar,
+				user: req.user.id,
+			}
+
+			post.comments.unshift(newComment)
+
+			await post.save()
+
+			return res.json(post.comments)
+		} catch (error) {
+			return res.status(500).json({
+				msg: 'Server Error',
+			})
+		}
+	}
+)
+
+/**
+ * @route DELETE  api/posts/comment/:id/:comment_id
+ * @desc  Delete a comment
+ * @access Private
+ */
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+	try {
+		//get the post
+		const post = await Post.findById(req.params.id)
+		//pul out comment
+		const comment = post.comments.find(
+			(comment) => comment.id === req.params.comment_id
+		)
+		if (!comment) {
+			return res.status(404).json({ msg: 'Comment not found' })
+		}
+		//check user
+		if (comment.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: 'USer not authorized' })
+		}
+
+		const removeIndex = post.comments
+			.map((comment) => comment.user.toString())
+			.indexOf(req.user.id)
+		post.comments.splice(removeIndex, 1)
+
+		await post.save()
+		return res.json(post.comments)
+	} catch (error) {
+		return res.status(500).json({
+			msg: 'Server Error',
+		})
+	}
+})
 module.exports = router
