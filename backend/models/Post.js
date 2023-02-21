ca825b9139870109883fe3b9d1c485e3369a30f6
dereSundayT <toluwanisundaydere@gmail.com>
2023-02-21 13:06:42 +0100

    created post model

---
+++ backend/models/Post.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+const PostSchema = new mongoose.Schema({
+	user: {
+		type: mongoose.Schema.Type.ObjectId,
+		ref: 'users',
+	},
+	text: {
+		type: String,
+		required: true,
+	},
+	name: {
+		type: String,
+	},
+	avatar: {
+		type: String,
+	},
+	likes: [
+		{
+			user: {
+				type: mongoose.Schema.Type.ObjectId,
+				ref: 'users',
+			},
+		},
+	],
+	comments: [
+		{
+			user: {
+				type: mongoose.Schema.Type.ObjectId,
+				ref: 'users',
+			},
+			text: {
+				type: String,
+				required: true,
+			},
+			avatar: {
+				type: String,
+			},
+			date: {
+				type: Date,
+				default: Date.now,
+			},
+		},
+	],
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+})
+
+module.exports = Post = mongoose.model('post', PostSchema)
