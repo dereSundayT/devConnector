7f500034c9928790942d37e755516e44cf9cac20
dereSundayT <toluwanisundaydere@gmail.com>
2023-02-21 13:34:43 +0100

    added new method to add post

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -1,16 +1,44 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
+const auth = require('../../middleware/auth')
 
-
+const Post = require('../../models/Post')
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
 
 /**
- * @route GET api/post
- * @desc  Test Route
- * @access Public
+ * @route POST api/posts
+ * @desc  Create a post
+ * @access Private
  */
-router.get('/',(req,res) => {
-    //
-    res.json('hello')
-})
+router.post(
+	'/',
+	[auth, [check('text', 'Text is required').not().isEmpty()]],
+	async (req, res) => {
+		const errors = validationResult(req)
 
-module.exports = router;
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() })
+		}
+
+		try {
+			const user = await User.findById(req.user.id).select('-password')
+			const newPost = {
+				text: req.body.text,
+				name: user.name,
+				avatar: user.avatar,
+				user: req.user.id,
+			}
+			const post = await Post.save(newPost)
+
+			return res.json(post)
+		} catch (error) {
+			return res.status(500).json({
+				msg: 'Server Error',
+			})
+		}
+	}
+)
+
+module.exports = router
