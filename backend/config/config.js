30f838282298a2011f8c02d253290675a4db26b2
dereSundayT <toluwanisundaydere@gmail.com>
2023-02-21 08:38:24 +0100

    worked on getting user's repo from github

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,12 +1,13 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
+const dotenv = require('dotenv')
+dotenv.config()
 
 const appConfig = {
-    'appPortNo' : process.env.PORT || 5000,
-    'db_user_name' : process.env.MONGO_DB_USERNAME,
-    'db_password' : process.env.MONGO_DB_PASSWORD,
-    'jwtSecret' : process.env.JWT_SECRET
+	appPortNo: process.env.PORT || 5000,
+	db_user_name: process.env.MONGO_DB_USERNAME,
+	db_password: process.env.MONGO_DB_PASSWORD,
+	jwtSecret: process.env.JWT_SECRET,
+	github_client_secret: process.env.GITHUB_CLIENT_SECRET,
+	github_client_id: process.env.GITHUB_CLIENT_ID,
 }
 
 module.exports = appConfig
--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+const request = require('request')
+const config = request('config')
+
 const auth = require('../../middleware/auth')
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
@@ -311,6 +314,37 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 	} catch (err) {
 		console.error(err.message)
 		res.status(500).send('Server Error')
+	}
+})
+
+/**
+ * @route GET api/profile/github/:username
+ * @desc  GET user repos from github
+ * @access Public
+ */
+
+router.get('/github/:username', async (req, res) => {
+	try {
+		const options = {
+			uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5
+			&sort=created:asc
+			&client_id=${config.get('github_client_id')}
+			&client_secret=${config.get('github_client_secret')}`,
+			method: 'GET',
+			headers: { 'user-agent': 'node.js' },
+		}
+
+		request(options, (error, response, body) => {
+			if (error) console.error(error)
+
+			if (response.statusCode !== 200)
+				res.status(404).json({ msg: 'github profile not found' })
+
+			return res.json(JSON.parse(body))
+		})
+	} catch (err) {
+		console.log(err.message)
+		res.status(500).send('Server error')
 	}
 })
 
